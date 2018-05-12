edc811c3331650a383b1c0ea2aa821b812816aab
hsoni134 <37224862+hsoni134@users.noreply.github.com>
2018-05-13 00:36:39 +0200

    Login_ProtractorTest
    This is simple Login Test using Angular JS objects

---
+++ Login/example_spec.js
@@ -0,0 +1,12 @@
+describe('Testing Login Application', function() {
+  it('Should able to login by username, password ' , function() {
+    browser.get('http://www.way2automation.com/angularjs-protractor/registeration/#/login');
+    element(by.id('username')).sendKeys('angular');
+	element(by.id('password')).sendKeys('password');
+	element(by.id('formly_1_input_username_0')).sendKeys('HarshSoni');
+	element(by.className('btn btn-danger')).click();
+	
+	expect(element(by.tagName('p')).getText()).toBe('You\'re logged in!!');
+	});
+  
+});
